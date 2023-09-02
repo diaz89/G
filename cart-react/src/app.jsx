a630fc2718d1c98225fffac5fd0a2c67aa01ac3d
diaz <diazgarcia89@gmail.com>
2023-09-01 22:22:37 -0600

    guia3

---
+++ cart-react/src/app.jsx
@@ -0,0 +1,11 @@
+import {Headers} from './components/Headers';
+import {ProductList} from './components/ProductList';
+function App() {
+return (
+<>
+<Headers></Headers>
+<ProductList></ProductList>
+</>
+);
+}
+export default App;
